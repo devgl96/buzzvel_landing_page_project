e0e6a86ef862c3c03fefb74a0927271c4eb7912b
devgl96 <georgelucas.dev@gmail.com>
2024-08-26 23:14:47 -0300

    UPDATE: Reason Card

--- src/components/ReasonCard/index.tsx
+++ src/components/ReasonCard/index.tsx
@@ -1,18 +1,34 @@
+import React from "react";
+import { BookIcon } from "../Icons/Book";
+import { MedalIcon } from "../Icons/Medal";
 import { RocketIcon } from "../Icons/Rocket";
+import { UsersIcon } from "../Icons/Users";
 import styles from "./reasoncard.module.css";
 
-export function ReasonCard() {
+export interface ReasonCardProps {
+  title: string;
+  description: string;
+  icon: string;
+}
+
+export function ReasonCard({ title, description, icon }: ReasonCardProps) {
+  const Icons = {
+    rocket: <RocketIcon />,
+    book: <BookIcon />,
+    medal: <MedalIcon />,
+    users: <UsersIcon />,
+  };
+
+  function renderIcon() {
+    return Icons[icon];
+  }
+
   return (
     <div className={styles.reasonCard}>
-      <div className={styles.reasonIcon}>
-        <RocketIcon />
-      </div>
+      <div className={styles.reasonIcon}>{renderIcon()}</div>
       <div className={styles.reasonContent}>
-        <h3>Innovation</h3>
-        <p>
-          Buzzvel encourages its employees to think outside the box and explore
-          new ideas.
-        </p>
+        <h3>{title}</h3>
+        <p>{description}</p>
       </div>
     </div>
   );
