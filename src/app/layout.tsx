b3fa947bdc4cc7cf7b803bd903e6713a307a03e0
devgl96 <georgelucas.dev@gmail.com>
2024-09-07 23:23:44 -0300

    ADD: Open Graph Tags to help SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          property="og:title"
+          content="Buzzvel Dev - Why I want to work at Buzzvel"
+        />
+        <meta
+          property="og:description"
+          content="I’m George Lucas, a passionate Full Stack Developer with a strong background in creating innovative web solutions. Let’s explore how can contribute to Buzzvel’s vision."
+        />
+        <meta
+          property="og:image"
+          content="../../public/macbook_desk_book.jpg"
+        />
+        <meta
+          property="og:url"
+          content="https://georgelucas-buzzvel-project.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
